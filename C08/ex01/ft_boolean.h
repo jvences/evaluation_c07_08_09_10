8196f3e0e95dd513f1df668292a84f4316c3b019
jvences <77075522+jvences@users.noreply.github.com>
2021-02-03 08:18:53 +0000

    Create ft_boolean.h

---
+++ C08/ex01/ft_boolean.h
@@ -0,0 +1,13 @@
+#ifndef FT_BOOLEAN.H
+#define FT_BOOLEAN.H
+
+typedef int t_bool;
+
+#define EVEN(n) n % 2 == 0
+#define TRUE 1
+#define FALSE 0
+#define EVEN_MSG "I have an even number of arguments.\n"
+#define ODD_MSG "I have an odd number of arguments.\n"
+#define SUCCESS 0
+
+#endif
